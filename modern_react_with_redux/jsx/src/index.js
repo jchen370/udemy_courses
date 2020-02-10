d02abe2ad87c30d769d808cffc48129104732c92
Jessica Chen <jchen4181@gmail.com>
2020-02-09 19:30:03 -0500

    Section 1 Project

--- modern_react_with_redux/jsx/src/index.js
+++ modern_react_with_redux/jsx/src/index.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+const App = () =>{
+    const buttonText = "Click me";
+    
+    return (
+        <div>
+            <label className="label" htmlFor="name">Enter Name: </label>
+            <input id="name" type="text"/>
+            <button style={{backgroundColor:"red", color: "white"}}>
+                {buttonText}
+            </button>
+        </div>
+    )
+    
+}
+
+ReactDOM.render(
+    <App />,
+    document.querySelector("#root")
+);
