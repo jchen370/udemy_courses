b0d026dabcb2abf3fb6f679bff81a34f5834a0a1
Jessica Chen <jchen4181@gmail.com>
2020-04-17 16:14:23 -0400

    .

--- ractive/projects/user_manager/src/app.js
+++ ractive/projects/user_manager/src/app.js
@@ -1,13 +1,22 @@
-var Ractive = require("ractive");
+// var Ractive = require("ractive");
+import Ractive from "../node_modules/ractive";
 Ractive.DEBUG = false;
 
-var ractive = new Ractive({
-    template: "{{greeting}}",
-    data:{
-        greeting: "Hello World"
-    }
+var UserList = Ractive.extend({
+    isolated:true,
+    template: require("../templates/userList.mustache"),
+    oninit: function(){
+        console.log("UserList Component Init...")
+    },
 });
 
-ractive.set("greeting", "Hey");
+var ractive = new Ractive({
+    el:"#app",
+    template: require('../templates/main.mustache'),
+    data:{
+        title: "User Manager"
+    },
+    components: {userlist: UserList}
 
-console.log(ractive.toHTML());
+});
+
--- ractive/projects/user_manager/webpack.config.js
+++ ractive/projects/user_manager/webpack.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     entry: './src/app.js',
     output: {
-        path: __dirname + '/dist',
+        path: '/dist',
         filename: 'app.bundle.js'
     },
     module: {
